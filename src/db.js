f77208e3c1ee81580d99e4aa58b3b237bd49b509
Lucas Bastos <lucas@lucasbastos.com>
2021-09-27 22:59:37 -0400

    Consolidate packages

--- src/db.js
+++ src/db.js
@@ -1,11 +1,3 @@
-//require("dotenv").config();
-
-// This makes sure you can read your .env file
-//const host = process.env.MYSQL_HOST;
-//const database = process.env.MYSQL_DB;
-
-//console.log(`connecting to database ${database} on ${host}`);
-
 require("dotenv").config();
 var mariadb = require('mariadb');
 
