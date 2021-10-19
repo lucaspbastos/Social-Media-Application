9c04aa1b737a93fd176a56c518e2ed6018e32382
Michael Galanaugh <michael.galanaugh11@gmail.com>
2021-10-18 21:03:13 -0400

    Fixing issues with foreign keys

--- create-database.sql
+++ create-database.sql
@@ -1,69 +1,64 @@
 create database if not exists CS490;
+use CS490;
 
-CREATE TABLE User(
-
-    ID int AUTO_INCREMENT UNIQUE NOT NULL,
+CREATE TABLE Users (
+    userID int AUTO_INCREMENT UNIQUE NOT NULL,
     UserName varchar(1000) UNIQUE NOT NULL,
     HashedSaltPepperPassword varchar(1000) NOT NULL,
     Salt varchar(1000) NOT NULL,
     AdminRole int NOT NULL,
-    PRIMARY KEY(ID)
-)
+    PRIMARY KEY(userID)
+);
 
-CREATE TABLE Post(
+CREATE TABLE SessionsTable (
+    userID int NOT NULL,
+    sessionString varchar(255),
+    sessionDatetime datetime,
+    PRIMARY KEY(userID),
+    FOREIGN KEY(userID) REFERENCES User(userID)
+);
 
+
+CREATE TABLE Posts (
     postID int AUTO_INCREMENT UNIQUE NOT NULL,
-    postUserID int not NULL,
+    userID int not NULL,
     fileNames varchar(1000),
     postText varchar(1000),
     postDate datetime NOT NULL,
     blockStatus int NOT NULL,
     PRIMARY KEY(postID),
-    FOREIGN KEY(postUserID) REFERENCES User(ID)
+    FOREIGN KEY(userID) REFERENCES User(userID)
 );
 
-CREATE TABLE Comment(
-
+CREATE TABLE Comments (
     commentID int AUTO_INCREMENT UNIQUE NOT NULL,
-    commentUserID int not NULL,
-    originalPostID int not NULL,
+    userID int not NULL,
+    postID int not NULL,
     commentText varchar(1000),
     fileNames varchar(1000),
     commentDate datetime NOT NULL,
     blockStatus int NOT NULL,
     PRIMARY KEY(commentID),
-    FOREIGN KEY(commentUserID) REFERENCES User(ID)
-    FOREIGN KEY(originalPostID) REFERENCES Post(postID)
-
+    FOREIGN KEY(userID) REFERENCES Users(userID),
+    FOREIGN KEY(postID) REFERENCES Posts(postID)
 );
 
-CREATE TABLE Messages(
+CREATE TABLE Threads (
+    threadID int AUTO_INCREMENT UNIQUE NOT NULL,
+    threadName varchar(255) NOT NULL,
+    userIDs varchar(1000) NOT NULL,
+    PRIMARY KEY(threadID)
+);
+
+
+CREATE TABLE Messages (
     messageID int AUTO_INCREMENT UNIQUE NOT NULL,
-    senderUserID int NOT NULL,
+    threadID int NOT NULL,
+    userID int NOT NULL,
     recipientUserIDs varchar(1000) NOT NULL,
     messageText varchar(1000),
     messageDate datetime NOT NULL,
     PRIMARY KEY(messageID),
-    FOREIGN KEY(senderUserID) REFERENCES User(ID)
-
+    FOREIGN KEY(userID) REFERENCES Users(userID),
+    FOREIGN KEY(threadID) REFERENCES Threads(threadID)
 );
-
-CREATE TABLE SessionsTable(
-    sessionUserID int NOT NULL,
-    sessionString varchar(255),
-    sessionDatetime datetime,
-    PRIMARY KEY(sessionUserID),
-    FOREIGN KEY(sessionUserID) REFERENCES User(ID)
-
-);
-
-CREATE TABLE Thread(
-    threadID int AUTO_INCREMENT NOT NULL,
-    threadName varchar(255) NOT NULL,
-    userIDs varchar(1000) NOT NULL,
-    lastMessageID int NOT NULL,
-    PRIMARY KEY(threadID),
-    FOREIGN KEY(lastMessageID) REFERENCES Messages(messageID)
-
-);
-
