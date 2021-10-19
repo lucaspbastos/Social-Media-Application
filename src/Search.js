517022943e7a711989967bf6e5d4f04d1014e371
tahmim52 <tahmim52@gmail.com>
2021-10-19 12:59:21 -0400

    updated search page

--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,23 @@ import { useState } from "react";
 import AuthData from './AuthData';
 import { Redirect } from 'react-router';
 import { Link } from "react-router-dom";
+import PostCards from './Posts/PostCards'
+import Comment from "./Posts/Comment";
 
 function Search() {
   const [searchVal, setsearchVal]=useState('');
+  //const[searchObject,setsearchObject]=useState('');
+
   if(AuthData.getAuth()!=="true" ){
     return <Redirect to="/"/>;
   }
   
+  const searchObject = { 
+    results: {
+      posts: [{ id: 1, imgUrl: "", text : "<i><strong>somedata</strong></i>", user: "bob", comments: [{id: 1, imgUrl: "", text: "comment1", user: "blah"}, { id: 2, text: "comment2", user: "blah"}]}, { id: 2, text : "blah blah blah", user: "bob", comments: [{id: 1, text: "comment1", user: "blah"}, { id: 2, text: "comment3", user: "comment4"}]}],
+      user: {userID: 1, username: "bob"}
+     }
+  } 
   
   function handleClick(event,val){
     event.preventDefault();
@@ -17,12 +27,13 @@ function Search() {
         /*fetch('/search', {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({val})
+            body: JSON.stringify({search: val})
 
           }).then(res => {
             return res.json();
           }).then(function(data) {
-            console.log(data.login)
+            console.log(data)
+            setsearchObject(data)
         })*/
     }
   }
@@ -42,23 +53,27 @@ function Search() {
             <input style={{height:"31.5px", width:"100px"}}type="submit" value="Search" onClick={(e)=> handleClick(e,searchVal)}/>
             </form>
         </div>
-        
+        <div>
+              { (Object.keys(searchObject.results.user).length===0 ) ? (<></>) : (<h2>{"user: "+searchObject.results.user.username}</h2>)}
+        </div>
+        {(Object.keys(searchObject.results.posts).length === 0) ? (<>{console.log("no posts")}</>) :
+            (
+            <div>
+              {searchObject.results.posts.map((index)=>
+                            <div key={index.id}>
+                                <PostCards id={index.id} caption={index.text} imgUrl={index.imgUrl}/>
+                                {index.comments.map((idx)=>
+                                    <div key={idx.id }>
+                                        <Comment id={index.id} comment={idx.text} />
+                                    </div>
+                                )}
+                                <br/>
+                            </div>
+                        )}
+            </div>
+            )}
     </div>
   );
 }
 
 export default Search;
-/*
-<div>
-                    {dataObject.posts.map((index)=>
-                        <div key={index.id}>
-                            <PostCards id={index.id} caption={index.text} imgUrl={index.imgUrl}/>
-                            {index.comments.map((idx)=>
-                                <div key={idx.id }>
-                                    <Comment id={index.id} comment={idx.text} />
-                                </div>
-                            )}
-                            <br/>
-                        </div>
-                    )}               
-                </div>*/
