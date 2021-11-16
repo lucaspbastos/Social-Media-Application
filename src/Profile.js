7c398de5fa24ea8aaaa131461e4f68591ac7bdbe
Lucas Bastos <lucas@lucasbastos.com>
2021-11-16 16:43:52 -0500

    Add posts to profile

--- src/Profile.js
+++ src/Profile.js
@@ -13,11 +13,20 @@ import Posts from './Posts/Posts'
 import Messages from './Messaging/Messages'
 import Search from './Search'
 import { useState, useEffect } from 'react';
+import PostCards from './Posts/PostCards'
+import Comment from './Posts/Comment'
 
 function Profile({ requestedUserID }){
 
     const[userInfo, setUserInfo] = useState('');
-    const[userObject, setUserObject] = useState({});
+    const[userObject, setUserObject] = useState({
+        user: {
+            username: '',
+            profilePicture: '',
+            followingList: [0],
+            posts: [],
+        }
+    });
 
     useEffect(async () => {
         fetch('http://localhost:3002/getUser', {
@@ -38,15 +47,27 @@ function Profile({ requestedUserID }){
 
 
     return(
-        
-        <div>
-            {console.log(userObject.user)}
+        <div style={{textAlign: "center"}}>
             <h1> Username </h1>
             <h2>{userObject.user.username}</h2>
-            <h1> Following List </h1>
-            <h2>{userObject.user.followingList}</h2>
+            <h1> Following:</h1>
+            <h2>{JSON.parse(userObject.user.followingList).length-1}</h2>
             <h1> Profile Picture </h1>
             <h2>{userObject.user.profilePicture}</h2>
+            {console.log(userObject.user.posts)}
+            <div>
+            {userObject.user.posts.map((post)=>
+                (<div key={post.postID}>
+                    <PostCards id={post.postID} caption={post.postText} imgUrl={post.postAttachments}/>
+                    {post.comments.map((comment)=>
+                        <div key={comment.commentID }>
+                            <Comment id={comment.commentID} comment={comment.commentText} />
+                        </div>
+                    )}
+                    <br/>
+                </div>)
+            )}
+            </div>
         </div>
         
     )
